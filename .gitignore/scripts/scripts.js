4591b7c34a0d468887bd8e24f8545d217b47ede2
CHVRLY <smz@avgservers.fr>
2018-07-30 15:47:46 +0200

    Update scripts.js

--- .gitignore/scripts/scripts.js
+++ .gitignore/scripts/scripts.js
@@ -2,4 +2,3 @@ const images = require("./images.js");
 const heure = require("./heure.js");
 const erreurs = require("./erreurs.js");
 const random_reponses = require("./random_reponses.js");
-const clear = require("./clear.js");
